02f1012f90e859a69a676259011ebaee0a238f24
ParkGisoon <aosscv@naver.com>
2020-07-12 16:47:07 +0900

    Security 초기 설정

--- src/main/java/com/springStudy/SpringStudyApplication.java
+++ src/main/java/com/springStudy/SpringStudyApplication.java
@@ -2,18 +2,12 @@ package com.springStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
-	}
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringStudyApplication.class);
 	}
 
 }
