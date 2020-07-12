e17625bb9264fd2b957cf32dad672285241769b9
juinsun <qwe506@naver.com>
2020-07-12 12:37:42 +0900

    TEST

--- src/main/java/com/springStudy/SpringStudyApplication.java
+++ src/main/java/com/springStudy/SpringStudyApplication.java
@@ -2,12 +2,18 @@ package com.springStudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringStudyApplication.class);
 	}
 
 }
